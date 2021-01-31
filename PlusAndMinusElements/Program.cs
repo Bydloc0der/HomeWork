211f200af43e41cbf275b996174f9da20e2a38d3
NurMan <elnurmaniac@gmail.com>
2021-01-31 20:50:35 +0300

    Сделал 1 задание. Второе - доламываю.

---
+++ PlusAndMinusElements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlusAndMinusElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4, -5, -7, 2, 3 };
+            methods(nums);
+        }
+
+        static void methods(int[] plus)
+        {
+            int[] plusnumbers;
+            int[] minusnumbers;
+            int alpha = 0;
+            int omega = 0;
+
+            for (int a = 0; a < plus.Length; ++a)
+            {
+                if (plus[a] < 0)
+                {
+                    plusnumbers[omega] = plus[a];
+                }
+            }
+        }
+    }
+}
--- Tested/Program.cs
+++ Tested/Program.cs
@@ -10,7 +10,35 @@ namespace Tested
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Zdarova g0vnyk");
+            int[] nums = { 4, 7, 2, 4, 3, 5, 1, 0, 1, 1, 8,1 };
+            FindBigOne(nums);
+            Console.ReadKey();
+        }
+
+        static void FindBigOne(int[] method)
+        {
+            int counter = 0;
+            int result = 0;
+
+            for (int a = 0; a < method.Length; ++a)
+            {
+                int clones = 0;
+
+                for (int b = 0; b < method.Length; ++b)
+                {
+
+                    if ((method[a] == method[b]) && a != b)
+                    {
+                        ++clones;
+                    }
+
+                    if (clones > counter)
+                    {
+                        result = a;
+                    }
+                }
+            }
+            Console.WriteLine($"{method[result]}");
         }
     }
 }
