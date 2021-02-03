438da71c32a5dd9919ac757138ca34df1988a61e
NurMan <elnurmaniac@gmail.com>
2021-02-03 23:57:57 +0300

    @shawdown

--- PlusAndMinusElements/Program.cs
+++ PlusAndMinusElements/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PlusAndMinusElements
 {
@@ -19,13 +15,13 @@ namespace PlusAndMinusElements
         {
             if (nums == null)
             {
-                Console.WriteLine("Введены не верные данные");
+                Console.WriteLine("Введены не верные данные. null - не является числом.");
                 Console.ReadKey();
                 Environment.Exit(0);
             }
             else if (nums.Length <= 0)
             {
-                Console.WriteLine("Введены не верные данные");
+                Console.WriteLine("Отсутствуют данные.");
                 Console.ReadKey();
                 Environment.Exit(0);
             }
@@ -38,38 +34,38 @@ namespace PlusAndMinusElements
                     Environment.Exit(0);
                 }
             }
-            int positiveElement = 0; // переменная отражает количество положительных элементов, в заложенном массиве.
-            int negativeElement = 0;  // переменная отражает количество отрицательных элементов, в заложенном массиве.
+            int positiveElementCount = 0; // переменная отражает количество положительных элементов, в заложенном массиве.
+            int negativeElementCount = 0;  // переменная отражает количество отрицательных элементов, в заложенном массиве.
 
             for (int b = 0; b < nums.Length; ++b)
             {
                 if (nums[b] > 0)
                 {
-                    ++positiveElement;
+                    ++positiveElementCount;
                 }
 
                 else if (nums[b] < 0)
                 {
-                    ++negativeElement;
+                    ++negativeElementCount;
                 }
             }
-            int[] plusNumbers = new int[positiveElement]; // массив с положительными элементами
-            int[] minusNumbers = new int[negativeElement]; // массив с отрицательными элементами
-            int positiveCounter = 0; // переменная, дающая  номер положительному элементу
-            int negativeCounter = 0; // переменная, дающая  номер отрицательному элементу
+            int[] plusNumbers = new int[positiveElementCount]; // массив с положительными элементами
+            int[] minusNumbers = new int[negativeElementCount]; // массив с отрицательными элементами
+            int nextPositiveElementIndex = 0; // переменная, дающая  номер положительному элементу
+            int nextNegativeElementIndex = 0; // переменная, дающая  номер отрицательному элементу
 
             for (int a = 0; a < nums.Length; ++a)
             {
                 if (nums[a] < 0)
                 {
-                    minusNumbers[negativeCounter] = nums[a];
-                    ++negativeCounter;
+                    minusNumbers[nextNegativeElementIndex] = nums[a];
+                    ++nextNegativeElementIndex;
                 }
 
                 else if (nums[a] > 0)
                 {
-                    plusNumbers[positiveCounter] = nums[a];
-                    ++positiveCounter;
+                    plusNumbers[nextPositiveElementIndex] = nums[a];
+                    ++nextPositiveElementIndex;
                 }
             }
             Console.Write("[");
