4e787ac63d353a14756f68260784f5d95f2a85f2
ElNurMan <elnurmaniac@gmail.com>
2021-10-17 22:17:53 +0300

    @shawdown. Исправил отступы/строки, метод назвал корректно, индексы заполнены верно.

--- ListOfThreeHundredNumbers/Program.cs
+++ ListOfThreeHundredNumbers/Program.cs
@@ -7,41 +7,35 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int[] arrayOfThreeDigitNumbers;///массив, в котором будет количество трехзначных чисел, уменьшающихся в 9 раз, при зачеркивании средней цифры
-            int numberOfDigitsDecreasingNineTimes = SortThreeDigitNumbers(out arrayOfThreeDigitNumbers);
+            int numberOfDigitsDecreasingNineTimes = SearchThreeDigitNumbers(out arrayOfThreeDigitNumbers);
             Console.WriteLine($"Количество трёхзначных чисел: {numberOfDigitsDecreasingNineTimes}");
             Console.ReadKey();
         }
         /// <summary>
-        /// метод возвращает одномерный массив.
+        /// метод возвращает кол-во чисел трёхзначных, которые меньше в 9 раз, при зачеркивании средней цифры.
         /// </summary>
         /// <param name="arrayofthreedigitnumbers">массив, который заполнится количестом трёхзначных чисел, уменьшающихся в 9 раз, при зачеркивании средней цифры</param>
-        static int SortThreeDigitNumbers(out int[] arrayOfThreeDigitNumbers)
+        static int SearchThreeDigitNumbers(out int[] arrayOfThreeDigitNumbers)
         {
             int numberOfDigitsDecreasingNineTimes = 0;//переменная, отражающая количество трехзначных чисел, уменьшающихся в 9 раз, при зачеркивании средней цифры
-            Console.Write("Трёхзначные числа, которые уменьшаются в 9 раз, при зачеркивании средней цифры:\n");
 
             for (int a = 100; a < 999; ++a)
             {
-
                 if (a % 9 == 0 && a / 100 * 10 + a % 10 == a / 9)
                 {
-                    Console.WriteLine($"-{a}");
                     ++numberOfDigitsDecreasingNineTimes;
                 }
             }
+
             arrayOfThreeDigitNumbers = new int[numberOfDigitsDecreasingNineTimes];
+            int indexOfArray = numberOfDigitsDecreasingNineTimes - 1; // переменная отвечает за индекс в переменной, для присвоения значения.
 
-
-            for (int b = 100; b < 999; ++b)
+            for (int b = 999; b > 100 ; --b)
             {
-
                 if (b % 9 == 0 && b / 100 * 10 + b % 10 == b / 9)
                 {
-
-                    for (int c = 0; c < numberOfDigitsDecreasingNineTimes; ++c)
-                    {
-                        arrayOfThreeDigitNumbers[c] = b;
-                    }
+                    arrayOfThreeDigitNumbers[indexOfArray] = b;
+                    --indexOfArray;
                 }
             }
             return numberOfDigitsDecreasingNineTimes;
